6563e0f400cd04685b64da2ea42a77f7cad8059a
Fpliz <felipesouzadias220315@gmail.com>
2026-04-03 16:11:53 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://githubusercontent.com'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 
 local Window = Rayfield:CreateWindow({
